6d3d29787f8d20c120cffe02dbcc9f43ee40dec0
Max Vashchenko <maxvaschenko@gmail.com>
2020-01-04 10:28:05 +0200

    +add: added textarea

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./style.scss";
+import {Link} from "react-router-dom";
 
 export const Home = () => {
+  const [initialText, changeInitialText] = useState('');
+  useEffect(() => {
+    document.title = "Home";
+  }, []);
+
+  const changeTextAreaValue = e => {
+    changeInitialText(e.target.value);
+  };
+
+  console.log(initialText);
+
   return (
     <div className="Home">
-      <p>Home</p>
+      <textarea
+        name="text-area"
+        id="initial-text-area"
+        cols="80"
+        rows="10"
+        value={initialText}
+        onChange={changeTextAreaValue}
+      />
+      <Link to={"/layout"}>
+        <button>LAYOUT</button>
+      </Link>
     </div>
   );
 };
